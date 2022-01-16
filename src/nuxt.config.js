f08c3b2078d3042535e50795cdfcba66c18acba9
Mohammad Daffa <m.daffa100@yahoo.com>
2022-01-16 17:26:26 +0700

    update hosting site name to be invitree-app-sample

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - firebase-ssr',
-    title: 'firebase-ssr',
+    titleTemplate: '%s - invitree-app-sample',
+    title: 'invitree-app-sample',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
